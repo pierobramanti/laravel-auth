4df6056c674a57776bf612694090eeda4565e52b
pierobramanti <piero.bramanti94@gmail.com>
2024-10-04 17:02:18 +0200

    [UPD] created dashboard.blade

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    @vite(['resources/js/app.js'])
+
+</head>
+<body>
+    <div>
+        <div class="sidebar">
+
+        </div>
+        <div class="content">
+            <div class="header">
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
