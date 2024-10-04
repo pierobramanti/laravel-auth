014267b22dc8f9fc42255c9b636842351927e9b8
pierobramanti <piero.bramanti94@gmail.com>
2024-10-04 16:30:58 +0200

    [UPD] created columns and launched command migrate

--- database/migrations/2024_10_04_142306_create_projects_table.php
+++ database/migrations/2024_10_04_142306_create_projects_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('description') ->nullable();
+            $table->date('date'); 
+
             $table->timestamps();
         });
     }
