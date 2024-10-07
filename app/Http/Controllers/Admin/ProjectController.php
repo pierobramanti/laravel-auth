1aa5f15a29d5e7287b5b2cbd84133b905f5384a2
pierobramanti <piero.bramanti94@gmail.com>
2024-10-07 19:24:09 +0200

    [UPD] Add request imports to ProjectController

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
 
 class ProjectController extends Controller
 {
