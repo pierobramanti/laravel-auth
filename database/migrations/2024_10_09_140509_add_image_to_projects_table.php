d2907fe7c50cbfe86cc7f46a2480ce3fa7ea96ec
pierobramanti <piero.bramanti94@gmail.com>
2024-10-09 16:13:58 +0200

    [UPD] created migration

--- database/migrations/2024_10_09_140509_add_image_to_projects_table.php
+++ database/migrations/2024_10_09_140509_add_image_to_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+           $table->string('image')->nullable()->after('slug');
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropColumn('image');
         });
     }
 };
