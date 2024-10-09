6ab87b5fae60a78a6c38353dbf7a1856e8fc75a3
pierobramanti <piero.bramanti94@gmail.com>
2024-10-09 17:42:01 +0200

    [UPD] implement image editing and preview in project edit form

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -17,7 +17,7 @@
                     </ul>
                 </div>
             @endif
-            <form action="{{ route('admin.projects.update', ['project'=>$project->id]) }}" method="post">
+            <form action="{{ route('admin.projects.update', ['project' => $project->id]) }}" method="post" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
                 <div class="row">
@@ -31,6 +31,17 @@
                     <div class="col-12 mb-3">
                         <label for="description" class="control-label">Descrizione</label>
                         <textarea name="description" id="description" class="form-control" rows="4" placeholder="Inserisci una descrizione (opzionale)">{{ old('description', $project->description) }}</textarea>
+                    </div>
+                    <div class="col-12 mb-3">
+                        @if($project->image != null)
+                            <img src="{{ asset('storage/' . $project->image) }}" class="img-fluid rounded" alt="{{ $project->title }}">
+                        @else
+                            <img src="https://via.placeholder.com/600x400" alt="Immagine del progetto">
+                        @endif 
+                    </div>
+                    <div class="col-12 mb-3">
+                        <label for="image" class="control-label">Immagine</label>
+                        <input type="file" name="image" id="image" class="form-control">
                     </div>
                     <div class="col-12 mb-3">
                         <label for="date" class="control-label">Data</label>
@@ -49,4 +60,3 @@
 </div>
 
 @endsection
-
